96edf2f386a2a1d2d7600ba7d1444843b3bb837a
Brooke-Estes <brooke@estes-family.net>
2014-12-20 22:57:29 -0600

    Adding missed reference list

---
+++ source/Microsoft.AspNet.Identity.RavenDB/RoleReferenceList.Generic.cs
@@ -0,0 +1,26 @@
+﻿namespace Microsoft.AspNet.Identity.RavenDB
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// A generic class that expects an implementation of Identity's <see cref="IRole"/>.
+    /// 
+    /// The idea is to keep the roles, and any other reference data, in a single document.
+    /// A good candidate for a reference list is data that is potentially heavier on the read operations 
+    /// with infrequent inserts and updates.
+    /// </summary>
+    /// <typeparam name="TRole">The type of the role.</typeparam>
+    public class RoleReferenceList<TRole>
+        where TRole : IRole
+    {
+        public RoleReferenceList(string documentKey)
+        {
+            Id = documentKey;
+        }
+
+        public string Id { get; private set; }
+
+        public List<TRole> Roles { get; set; }
+
+    }
+}
