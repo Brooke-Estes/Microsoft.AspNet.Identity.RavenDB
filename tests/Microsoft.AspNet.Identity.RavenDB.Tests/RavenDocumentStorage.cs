1ed924762650ca443355608a0d22a35d449241b3
Brooke-Estes <brooke@estes-family.net>
2014-12-25 00:12:48 -0600

    Added tests around IUserStore<>.

---
+++ tests/Microsoft.AspNet.Identity.RavenDB.Tests/RavenDocumentStorage.cs
@@ -0,0 +1,39 @@
+﻿namespace Microsoft.AspNet.Identity.RavenDB.Tests
+{
+    using NUnit.Framework;
+    using Raven.Client;
+    using Raven.Client.Embedded;
+
+    public class RavenDocumentStorage
+    {
+        protected IDocumentStore RavenDocumentStore { get; private set; }
+
+        protected IAsyncDocumentSession AsyncDocumentSession
+        {
+            get { return CreateAsyncSession(); }
+        }
+
+        [TestFixtureSetUp]
+        protected void SetStorageContext()
+        {
+            RavenDocumentStore = new EmbeddableDocumentStore()
+            {
+                RunInMemory = true
+            };
+
+            RavenDocumentStore.Initialize();
+            RegisterConventions();
+        }
+
+        protected virtual void RegisterConventions() { }
+
+        private IAsyncDocumentSession CreateAsyncSession()
+        {
+            var session = RavenDocumentStore.OpenAsyncSession();
+            session.Advanced.UseOptimisticConcurrency = true;
+            return session;
+        }
+
+       
+    }
+}
